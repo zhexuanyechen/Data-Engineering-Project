b9c83c2c1f77ac5906eb91b3c7a2d996e2a7f572
Sam Galle <sam.galle@student.hogent.be>
2021-10-17 17:21:25 +0200

    add loop for datasheets folder

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Data-Engineering-Project
 ## This python script enables us to automaticly update our database
+
+If you want to run this script you'll need to add a config.py file with the database connection information with the following format
+
+```
+host=[Hostaddress of the DB]
+user=[Username]
+password=[Password]
+database=[Name of the DB]
+```
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import config
 from sqlalchemy import create_engine
 import pandas as pd
+import os
 
 engine = create_engine(f'mysql://{config.user}:{config.password}@{config.host}/{config.database}')
 
+for filename in os.listdir("datasheets/"):
+    print(filename)
+    df= pd.read_excel(f"datasheets/{filename}")
+    df.to_sql(filename,con=engine, chunksize=10000,method='multi')
 
 
 
