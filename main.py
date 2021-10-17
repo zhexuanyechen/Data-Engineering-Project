c9a7bcbc49438628cc1cf4cfebcf1e253042c8fe
Sam Galle <sam.galle@student.hogent.be>
2021-10-17 20:09:53 +0200

    add site files & automatic downloading for datasheets

--- main.py
+++ main.py
@@ -2,13 +2,30 @@ import config
 from sqlalchemy import create_engine
 import pandas as pd
 import os
+import requests as r
+import sites
 
+# Creating the engine to talk with the database
 engine = create_engine(f'mysql://{config.user}:{config.password}@{config.host}/{config.database}')
+directory = 'datasheets/'
 
-for filename in os.listdir("datasheets/"):
-    print(filename)
-    df= pd.read_excel(f"datasheets/{filename}")
-    df.to_sql(filename,con=engine, chunksize=10000,method='multi')
+# Pulling all datafiles from the specified websites
+for name in sites.sites:
+    request = r.get(sites.sites[name])
+    open(f'{directory}{name}','wb').write(request.content)
+
+# Creating tables for all datafiles in the folder datasheets
+for filename in os.listdir(directory):
+    if filename.endswith('.csv'):
+        df= pd.read_csv(f"{directory}{filename}")
+    elif filename.endswith('.xlsx'):
+        df= pd.read_excel(f"{directory}{filename}")        
+    df.to_sql(os.path.splitext(filename)[0],con=engine, if_exists='replace',chunksize=10000,method='multi')
+
+# Clearing the directory that holds the datafiles 
+for files in os.listdir(directory):
+    os.remove(os.path.join(directory,files))
+
 
 
 
---
+++ sites.py
@@ -0,0 +1,3 @@
+sites = {
+    'vaccinationByMunicipality.csv': "https://epistat.sciensano.be/data/COVID19BE_VACC_MUNI_CUM.csv"
+}
