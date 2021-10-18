41132861e7b814c20394153540a1701e86b1c93b
Sam Galle <sam.galle@student.hogent.be>
2021-10-18 16:00:22 +0200

    fix extension of populationDensity

--- sites.py
+++ sites.py
@@ -2,5 +2,5 @@ sites = {
     'vaccinationByMunicipality.csv': "https://epistat.sciensano.be/data/COVID19BE_VACC_MUNI_CUM.csv",
     'populationInfo.xlsx':'https://statbel.fgov.be/sites/default/files/files/opendata/bevolking%20naar%20woonplaats%2C%20nationaliteit%20burgelijke%20staat%20%2C%20leeftijd%20en%20geslacht/TF_SOC_POP_STRUCT_2021.xlsx',
     'incomeStatistics.xlsx':'https://statbel.fgov.be/sites/default/files/files/opendata/arbeid/TF_PSNL_INC_TAX_MUNTY.xlsx',
-    'populationDensity':'https://statbel.fgov.be/sites/default/files/files/documents/bevolking/5.11%20Bevolkingsdichtheid/Pop_density_en.xlsx',
+    'populationDensity.xlsx':'https://statbel.fgov.be/sites/default/files/files/documents/bevolking/5.11%20Bevolkingsdichtheid/Pop_density_en.xlsx',
 }
