a06e64be6eb6195029c8f8ae1ea84b82c52773e8
Sam Galle <sam.galle@student.hogent.be>
2021-10-25 11:07:57 +0200

    add functionality for multiple sheets

--- main.py
+++ main.py
@@ -4,27 +4,40 @@ import pandas as pd
 import os
 import requests as r
 import sites
-
+ 
 # Creating the engine to talk with the database
 engine = create_engine(f'mysql://{config.user}:{config.password}@{config.host}/{config.database}')
 directory = 'datasheets/'
 
 # Pulling all datafiles from the specified websites
+print("Pulling files")
 for name in sites.sites:
     request = r.get(sites.sites[name])
     open(f'{directory}{name}','wb').write(request.content)
 
 # Creating tables for all datafiles in the folder datasheets
+print("Creating Tables")
 for filename in os.listdir(directory):
-    if filename.endswith('.csv'):
+    if filename.endswith('.xlsx'):
+        sheets = pd.read_excel(f"{directory}{filename}",sheet_name=None)
+        if(len(sheets) > 1):
+            print(f"{filename} has more than 1 datasheet")
+            for sheet in sheets: 
+                df = pd.read_excel(f"{directory}{filename}",sheet_name=f"{sheet}")
+                df.to_sql(f"{filename}-{sheet}",con=engine, if_exists='replace',chunksize=10000,method='multi')
+        elif(len(sheets) == 1):       
+            df= pd.read_excel(f"{directory}{filename}")  
+            df.to_sql(os.path.splitext(filename)[0],con=engine, if_exists='replace',chunksize=10000,method='multi') 
+    elif filename.endswith('.csv'):          
         df= pd.read_csv(f"{directory}{filename}")
-    elif filename.endswith('.xlsx'):
-        df= pd.read_excel(f"{directory}{filename}")        
-    df.to_sql(os.path.splitext(filename)[0],con=engine, if_exists='replace',chunksize=10000,method='multi')
+        df.to_sql(os.path.splitext(filename)[0],con=engine, if_exists='replace',chunksize=10000,method='multi')  
+    
 
 # Clearing the directory that holds the datafiles 
-for files in os.listdir(directory):
-    os.remove(os.path.join(directory,files))
+print("Removing datafiles")
+for filename in os.listdir(directory):
+    if not filename.startswith('MP'):
+        os.remove(os.path.join(directory,filename))
 
 
 
