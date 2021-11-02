9ab42a95aa095674dd1e1096dda23cb5a779bed6
Sam Galle <sam.galle@student.hogent.be>
2021-11-02 17:27:21 +0100

    add query for population

--- sqlqueries.py
+++ sqlqueries.py
@@ -1,5 +1,6 @@
 queries = {
     "update vaccination_by_municipality set CUMUL = 5 where cumul = '<10'",
     "update vaccination_by_municipality set NIS5 = 0 where nis5 is null",
-    "alter table vaccination_by_municipality MODIFY COLUMN cumul int"
+    "alter table vaccination_by_municipality MODIFY COLUMN cumul int",
+    "update population_info set population_info.TX_DESCR_NL = REGEXP_REPLACE(TX_DESCR_NL,'\\(.*\\)','')"
 }
