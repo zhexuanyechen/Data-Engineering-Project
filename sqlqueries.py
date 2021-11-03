e4cf51b5902e931e7d54ca6a2d8a036a2cc9eef2
zhe xuan (oscar) <32307207+zhexuanyechen@users.noreply.github.com>
2021-11-03 17:47:28 +0100

    Some changes to the datatype and separate names

--- sqlqueries.py
+++ sqlqueries.py
@@ -3,12 +3,12 @@ queries = {
     "update vaccination_by_municipality set NIS5 = 0 where nis5 is null",
     "alter table vaccination_by_municipality MODIFY COLUMN cumul int",
     "update population_info set population_info.TX_DESCR_NL = REGEXP_REPLACE(TX_DESCR_NL,'\\(.*\\)','')",
-    "ALTER TABLE DataEngineerDB.populationDensity.xlsx-2020 
-    CHANGE COLUMN index index BIGINT(20) NULL ,
-    CHANGE COLUMN Population density by municipality 1st January 2020 Nis5 TEXT NULL ,
-    CHANGE COLUMN Unnamed: 1 MunicipalityFR TEXT NULL DEFAULT NULL ,
-    CHANGE COLUMN Unnamed: 2 MunicipalityNL TEXT NULL DEFAULT NULL ,
-    CHANGE COLUMN Unnamed: 3 Population TEXT NULL DEFAULT NULL ,
-    CHANGE COLUMN Unnamed: 4 Size TEXT NULL DEFAULT NULL ,
-    CHANGE COLUMN Unnamed: 5 Density TEXT NULL DEFAULT NULL ;"
+    "ALTER TABLE DataEngineerDB.population_density.xlsx-2020 
+    CHANGE COLUMN `index` `index` BIGINT(20) NULL ,
+    CHANGE COLUMN `Population density by municipality 1st January 2020` `Nis5` INTEGER NULL ,
+    CHANGE COLUMN `Unnamed: 1` `MunicipalityFR` TEXT NULL DEFAULT NULL ,
+    CHANGE COLUMN `Unnamed: 2` `MunicipalityNL` TEXT NULL DEFAULT NULL ,
+    CHANGE COLUMN `Unnamed: 3` `Population` BIGINT(20) NULL DEFAULT NULL ,
+    CHANGE COLUMN `Unnamed: 4` `Size` DOUBLE(10,2) NULL DEFAULT NULL ,
+    CHANGE COLUMN `Unnamed: 5` `Density` DOUBLE(10,2) NULL DEFAULT NULL ;"
 }
