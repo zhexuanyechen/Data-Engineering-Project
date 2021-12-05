3ceb7369c37d464deb003e07a1f3271c937232b5
Sam Galle <sam.galle@student.hogent.be>
2021-12-05 19:07:24 +0100

    removed a site & changed sqlqueries

--- sites.py
+++ sites.py
@@ -1,5 +1,5 @@
 sites = {
     'vaccination_by_municipality.csv': "https://epistat.sciensano.be/data/COVID19BE_VACC_MUNI_CUM.csv",
     'population_info.xlsx':'https://statbel.fgov.be/sites/default/files/files/opendata/bevolking%20naar%20woonplaats%2C%20nationaliteit%20burgelijke%20staat%20%2C%20leeftijd%20en%20geslacht/TF_SOC_POP_STRUCT_2021.xlsx',
-    'income_statistics.xlsx':'https://statbel.fgov.be/sites/default/files/files/opendata/arbeid/TF_PSNL_INC_TAX_MUNTY.xlsx',
+    'cases_municipality.csv':'https://epistat.sciensano.be/Data/COVID19BE_CASES_MUNI_CUM.csv'
 }
--- sqlqueries.py
+++ sqlqueries.py
@@ -1,9 +1,10 @@
 queries = {
-    "update vaccination_by_municipality set CUMUL = '5' where cumul = '<10'",
     "update vaccination_by_municipality set NIS5 = 0 where nis5 is null",
     "alter table vaccination_by_municipality MODIFY COLUMN cumul int",
     "update population_info set CD_PROV_REFNIS = 400 where CD_Dstr_refnis = 21000 ;",
     "update population_info set TX_PROV_DESCR_NL = 'Brussel' where cd_prov_refnis = 400",
     "update population_info set TX_PROV_DESCR_NL = 'other' where TX_PROV_DESCR_NL is null",
-    "update population_info set CD_PROV_REFNIS = 0 where CD_PROV_REFNIS is null"
-}
+    "update population_info set CD_PROV_REFNIS = 0 where CD_PROV_REFNIS is null",
+    "update vaccination_by_municipality set CUMUL = 5 where cumul = '<10'"  # If this line returns "Truncated incorrect DOUBLE value: '<10'   
+                                                                          # then it means it already set the correct double
+    }
